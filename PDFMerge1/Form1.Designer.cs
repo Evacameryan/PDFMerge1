74024225618a15350f2748828f0a63733c093c70
Committer2 <Evacameryan@gmail.com>
2019-06-20 14:59:50 -0400

    Commit from evacame

--- PDFMerge1/Form1.Designer.cs
+++ PDFMerge1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(48, 42);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 115);
+            this.button1.Size = new System.Drawing.Size(271, 195);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
